954d5a24d57321d8f16ec790e32bcac231b314e5
joannacollier <collj035@gmail.com>
2020-08-04 15:34:10 +0100

    eslint rules added in package

--- src/background.test.js
+++ src/background.test.js
@@ -4,7 +4,7 @@ import Background from '../src/background'
 describe('Background', () => {
 
   test('it creates the background Image', () => {
-    const image = new Background
+    var image = new Background
     expect(image.background).toBeInstanceOf(Image)
   })
 
