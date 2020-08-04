ddd4b6c7ccfac58b7aad27ba8f5c44924d606801
joannacollier <collj035@gmail.com>
2020-08-04 17:05:31 +0100

    score entry shows after gameover

--- src/game.js
+++ src/game.js
@@ -1,8 +1,6 @@
 import Score from '../src/score'
 import SFX from '../src/sfx'
-
 export default class Game {
-
   constructor(hero, game_width, game_height, input){
     this.game_width = game_width
     this.game_height = game_height
@@ -12,7 +10,6 @@ export default class Game {
     this.input = input
     this.sfx = new SFX();
   }
-
   draw(ctx){
     if(Score.infectionRate >= 1 || this.hero.position.y === 730){
     this.gameOver = true
@@ -21,7 +18,6 @@ export default class Game {
   } else {this.paused = false}
   this.gameStateText(ctx);
   }
-
   gameStateText(ctx){
     ctx.rect(0, 0, this.gameWidth, this.gameHeight);
     ctx.fillStyle = "rgba(0,0,0,0.3)";
@@ -32,6 +28,12 @@ export default class Game {
     if (this.gameOver){
       this.sfx.stayAtHome.play()
       ctx.fillText("Game Over", this.game_width / 2, this.game_height - 600);
+      var score = document.getElementById("score")
+      score.innerHTML = Score.distance
+      setTimeout(function(){
+        document.getElementById("gameScreen").setAttribute("style", "display:none")
+        document.getElementById("submitScore").setAttribute("style", "visibility:visible") 
+      }, 3000);
     } else if (this.paused){
       ctx.fillText("Paused", this.game_width / 2, this.game_height - 600);
     } else if (this.paused === false){
